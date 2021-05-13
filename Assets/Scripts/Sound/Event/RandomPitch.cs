c8809e3dcf52a359a13868b32c74ffaba26734cb
David <davidschipplock@web.de>
2021-05-13 16:57:12 +0200

    [FEATURE] Add random pitch script.

---
+++ Assets/Scripts/Sound/Event/RandomPitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPitch : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    [Header("Apply Random Pitch Range")]
+    [Range(-3f, 3f)] public float minPitchRange;
+    [Range(-3f, 3f)] public float maxPitchRange;
+
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ApplyRandomPitch()
+    {
+        audioSource.pitch = (Random.Range(minPitchRange,maxPitchRange));
+    }
+}
