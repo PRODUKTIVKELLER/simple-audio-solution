8576b4f4f13fd5df195fe5d20e524cf3445302cc
Stefan Bechert <stefan.bechert.inf@gmail.com>
2021-06-01 16:34:53 +0200

    [FEATURE/CLEAN-UP] Move 'Editor' folder out of 'Scripts' and call 'DontDestroyOnLoad' on 'SoundAccess' to prevent reloading on scene change.

--- Assets/Scripts/Sound/Access/SoundAccess.cs
+++ Assets/Scripts/Sound/Access/SoundAccess.cs
@@ -103,6 +103,8 @@ namespace Sound.Access
             else if (_instance == null)
             {
                 _instance = this;
+                transform.SetParent(null);
+                DontDestroyOnLoad(this);
                 Initialize();
             }
         }
